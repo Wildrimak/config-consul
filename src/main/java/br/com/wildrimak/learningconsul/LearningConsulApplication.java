67f7acc2463c2f461ded4c8fd7a3415d72181e47
wildrimak <wildrimak@gmail.com>
2022-03-08 23:19:24 -0300

    started project

---
+++ src/main/java/br/com/wildrimak/learningconsul/LearningConsulApplication.java
@@ -0,0 +1,13 @@
+package br.com.wildrimak.learningconsul;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearningConsulApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LearningConsulApplication.class, args);
+	}
+
+}

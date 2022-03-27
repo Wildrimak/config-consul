446066d123791749712d550c2992741a3e2aba24
wildrimak <wildrimak@gmail.com>
2022-03-27 00:28:22 -0300

    save

--- src/main/java/br/com/wildrimak/learningconsul/LearningConsulApplication.java
+++ src/main/java/br/com/wildrimak/learningconsul/LearningConsulApplication.java
@@ -1,13 +1,52 @@
 package br.com.wildrimak.learningconsul;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestClientException;
+import org.springframework.web.client.RestTemplate;
 
+import javax.naming.ServiceUnavailableException;
+import java.net.URI;
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@EnableDiscoveryClient
 @SpringBootApplication
 public class LearningConsulApplication {
 
+	@Value("${hello/hello-world}")
+	private String variable;
+
+	@Value("${hello.variable}")
+	private String other;
+
+	@Autowired
+	private DiscoveryClient discoveryClient;
+
+	@RequestMapping("/hello-world")
+	public String home() {
+		return "Hello World " + this.variable + ": " + this.other;
+	}
+
 	public static void main(String[] args) {
 		SpringApplication.run(LearningConsulApplication.class, args);
+	}
+
+	public String serviceUrl() {
+		List<ServiceInstance> list = discoveryClient.getInstances("hello");
+		if (list != null && list.size() > 0 ) {
+			return list.get(0).getUri().toString();
+		}
+		return null;
 	}
 
 }

64890dd701d4f5e5e437b5148cae90752d298d6a
wildrimak <wildrimak@gmail.com>
2022-03-29 01:28:23 -0300

    I win!

--- src/main/java/br/com/wildrimak/learningconsul/LearningConsulApplication.java
+++ src/main/java/br/com/wildrimak/learningconsul/LearningConsulApplication.java
@@ -4,49 +4,32 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.ServiceInstance;
 import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.client.RestClientException;
-import org.springframework.web.client.RestTemplate;
-
-import javax.naming.ServiceUnavailableException;
-import java.net.URI;
-import java.util.List;
-import java.util.Optional;
 
 @RestController
 @EnableDiscoveryClient
 @SpringBootApplication
 public class LearningConsulApplication {
 
-	@Value("${hello-world}")
-	private String variable;
+    @Value("${pronoun}")
+    private String pronoun;
 
-	@Value("${variable}")
-	private String other;
+    @Value("${yourName}")
+    private String yourName;
 
-	@Autowired
-	private DiscoveryClient discoveryClient;
+    @Autowired
+    private DiscoveryClient discoveryClient;
 
-	@RequestMapping("/hello-world")
-	public String home() {
-		return "Hello World " + this.variable + ": " + this.other;
-	}
+    @RequestMapping("/hello-world")
+    public String home() {
+        return "Olá " + this.pronoun + " " + this.yourName;
+    }
 
-	public static void main(String[] args) {
-		SpringApplication.run(LearningConsulApplication.class, args);
-	}
-
-	public String serviceUrl() {
-		List<ServiceInstance> list = discoveryClient.getInstances("hello");
-		if (list != null && list.size() > 0 ) {
-			return list.get(0).getUri().toString();
-		}
-		return null;
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(LearningConsulApplication.class, args);
+    }
 
 }
